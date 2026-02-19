05225cfc72fd4dbc538e982f9d55be46d2de61aa
Ana Paula Leão <91345737+analeao-dev@users.noreply.github.com>
2026-02-19 14:05:39 +0000

    feat: add AuthContext and integrate AuthProvider in main application

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState } from "react";
+
+type AuthContextType = {
+    acessToken: string | null;
+    setAccessToken: (token: string) => void;
+    user: { id: string; email: string } | null;
+    setUser: (user: AuthContextType['user']) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [accessToken, setAccessToken] = useState<string | null>(null);
+    const [user, setUser] = useState<AuthContextType['user']>(null);
+
+    const value = {
+        acessToken: accessToken,
+        setAccessToken,
+        user,
+        setUser
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAtuh = () => {
+    const context = useContext(AuthContext);
+    if (!context)
+        throw new Error("useAuth must be used within an AuthProvider");
+
+    return context;
+}
